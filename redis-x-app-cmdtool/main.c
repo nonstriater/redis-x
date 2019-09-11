cfb4bd864ccc4e42a21533d593c086c96f33ba17
冉文杰 <ranwenjie@didichuxing.com>
2019-09-11 20:15:19 +0800

    first commit

---
+++ redis-x-app-cmdtool/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  redis-x-app-cmdtool
+//
+//  Created by ranwenjie on 2019/6/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
